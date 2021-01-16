c2b3438d67affd0f0306a8adb49969d313b73198
David Grudl <david@grudl.com>
2021-01-16 14:22:05 +0100

    set up the repository as a course: Dependency Injection by Example
    Scaffolding for a chapter-per-concept course that starts with plain PHP and the
    three rules of dependency injection, and only reaches for Nette DI once the
    reader has written a container by hand and felt why maintaining it is tedious.
    
    Adds the root readme (the three rules, how to read a bad.php/good.php pair),
    AGENTS.md describing the two chapter shapes and the fixed domain classes, the
    license, and the tooling: phpstan, neon-lint, and bin/check-outputs.php, which
    verifies that the Output section of each chapter readme still matches what its
    scripts print - it pairs several scripts per chapter with their output blocks
    via "### bad.php" headings. composer check runs all three, and so does CI.
    
    Requirements are raised to PHP 8.4 and nette/di 3.2.6.

---
+++ bin/check-outputs.php
@@ -0,0 +1,101 @@
+<?php declare(strict_types=1);
+
+/**
+ * Verifies that the "## Output" section of every chapter readme still matches
+ * what the chapter's scripts actually print. Run it before committing:
+ * composer check
+ *
+ * A chapter with a single script has one fenced block under "## Output".
+ * A chapter with several scripts labels them with "### bad.php" subheadings.
+ */
+
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+	echo "Install dependencies using `composer install`\n";
+	exit(1);
+}
+
+$root = dirname(__DIR__);
+$failures = 0;
+
+foreach (glob("$root/[0-9][0-9]-*", GLOB_ONLYDIR) ?: [] as $dir) {
+	$name = basename($dir);
+	$readme = "$dir/readme.md";
+	$scripts = array_map('basename', glob("$dir/*.php") ?: []);
+
+	if (!$scripts || !is_file($readme)) {
+		echo "$name: no scripts or no readme.md\n";
+		$failures++;
+		continue;
+	}
+
+	if (!preg_match('~^## Output\s*$(.*?)(?=^## |\z)~ms', (string) file_get_contents($readme), $m)) {
+		echo "$name: no '## Output' section\n";
+		$failures++;
+		continue;
+	}
+
+	// Pair every fenced block with the script it documents: either the one
+	// named by the nearest "### script.php" heading, or the only script there is.
+	$section = $m[1];
+	preg_match_all('~(?:^###\s*(?<script>\S+?\.php)\s*$)?(?:(?!^###).)*?^```\w*\n(?<body>.*?)^```~ms', $section, $blocks, PREG_SET_ORDER);
+	if (!$blocks) {
+		echo "$name: '## Output' has no fenced block\n";
+		$failures++;
+		continue;
+	}
+
+	$expected = [];
+	foreach ($blocks as $block) {
+		$script = $block['script'] ?: (count($scripts) === 1 ? $scripts[0] : null);
+		if ($script === null) {
+			echo "$name: several scripts, so each output block needs a '### <script>.php' heading\n";
+			$failures++;
+			continue 2;
+		}
+		$expected[$script][] = $block['body'];
+	}
+
+	foreach ($expected as $script => $bodies) {
+		if (!in_array($script, $scripts, true)) {
+			echo "$name: readme documents $script, which does not exist\n";
+			$failures++;
+			continue;
+		}
+
+		exec(escapeshellarg(PHP_BINARY) . ' ' . escapeshellarg("$dir/$script") . ' 2>&1', $lines, $code);
+		$actual = implode("\n", $lines);
+		unset($lines);
+		if ($code !== 0) {
+			echo "$name/$script: exited with code $code\n";
+			$failures++;
+			continue;
+		}
+
+		// The readme may abridge the output: each chunk between `...` markers
+		// must appear in the real output, in order.
+		$offset = 0;
+		$missing = null;
+		foreach (preg_split('~^\s*\.\.\.\s*$~m', trim(implode("\n", $bodies))) ?: [] as $chunk) {
+			$chunk = trim($chunk, "\n");
+			if ($chunk === '') {
+				continue;
+			}
+			$pos = strpos($actual, $chunk, $offset);
+			if ($pos === false) {
+				$missing = $chunk;
+				break;
+			}
+			$offset = $pos + strlen($chunk);
+		}
+
+		if ($missing === null) {
+			echo "$name/$script: OK\n";
+		} else {
+			echo "$name/$script: readme shows output the script does not produce:\n";
+			echo preg_replace('~^~m', '    ', $missing) . "\n";
+			$failures++;
+		}
+	}
+}
+
+exit($failures === 0 ? 0 : 1);
